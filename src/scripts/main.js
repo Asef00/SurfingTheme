7e47aaffb5ca19d5eead08d943890120c038d79a
Asef <50338626+Asef00@users.noreply.github.com>
2022-05-04 00:30:13 +0430

    include jquery hero slider indicator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,33 @@
 /* eslint-env browser */
-(function() {
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function() {
+(function () {
+  "use strict";
+  document.addEventListener("DOMContentLoaded", function () {
     // Your custom JavaScript goes here
 
-    // Flickity init
-    var hero_slider = document.querySelector('.js-hero-slider');
-    if (hero_slider) {
-      new Flickity(hero_slider, {
-        pageDots: false,
-        autoPlay: true
-      });
+    // Flickity
+    var $heroSlider = $(".js-hero-slider").flickity({
+      pageDots: false,
+      autoPlay: 5000,
+      prevNextButtons: false,
+    });
+    var $index = $(".js-hero-index");
+    var $total = $(".js-hero-total");
+    var flkty = $heroSlider.data("flickity");
+    $total.text(flkty.slides.length);
+
+    function updateStatus() {
+      var cellNumber = flkty.selectedIndex + 1;
+      $index.text(cellNumber);
     }
+    updateStatus();
+    $heroSlider.on("select.flickity", updateStatus);
+
+    // Events
+    $(".js-prev-btn").on("click", function () {
+      $heroSlider.flickity("previous");
+    });
+    $(".js-next-btn").on("click", function () {
+      $heroSlider.flickity("next");
+    });
   });
 })();
