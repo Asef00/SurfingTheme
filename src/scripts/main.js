db929a27ab1c8899e38f89e834345c2adb46ead5
Asef <50338626+Asef00@users.noreply.github.com>
2022-05-03 19:23:31 +0430

    set hero slider overal style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,14 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function() {
     // Your custom JavaScript goes here
+
+    // Flickity init
+    var hero_slider = document.querySelector('.js-hero-slider');
+    if (hero_slider) {
+      new Flickity(hero_slider, {
+        pageDots: false,
+        autoPlay: true
+      });
+    }
   });
 })();
